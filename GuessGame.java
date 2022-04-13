e6b49d32c8651b0e114033076f56f97d0b65b03b
francis kamande <franciskamande2001@gmail.com>
2022-04-13 11:03:12 +0300

    The class definition and a method to launch the application with instance variables for the players

---
+++ GuessGame.java
@@ -0,0 +1,19 @@
+public class GuessGame{
+	//instance varibales for the three players objects
+	Player p1;
+	Player p2;
+	Player p3;
+
+	//It starts the game when called in the main method
+	public void startGame(){
+		//creating three players objects and assigning them to the three instance variables
+		p1 = new Player();
+		p2 = new Player();
+		p3 = new Player();
+
+		
+
+		
+
+	}
+}
